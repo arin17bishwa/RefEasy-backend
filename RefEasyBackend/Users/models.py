71e10daeefac14354bbf2a301ec7831b8c43a2db
bishwajit <arin17bishwa@gmail.com>
2022-05-20 16:39:55 +0530

    several changes

--- RefEasyBackend/Users/models.py
+++ RefEasyBackend/Users/models.py
@@ -1,16 +1,15 @@
-import uuid
+import shortuuid
 from django.contrib.auth.models import User, Group
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.db import models
 from .utils import get_group_name
-
 
 # Create your models here.
 
 
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid = models.CharField(primary_key=True, default=shortuuid.uuid, editable=False, max_length=32)
     email = models.EmailField(unique=True)
     role = models.CharField(choices=(('HR', 'HR'), ('NHR', 'Non-HR'), ('APP', 'Applicant')), max_length=5)
     referral_link = models.CharField(max_length=64, blank=True)
@@ -19,12 +18,12 @@ class Employee(models.Model):
     date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
     last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
 
-    def get_referral_link(self):
-        joined_name = ''.join(map(lambda x: x.title(), str(self.user.name).split(' ')))
-        return f'{joined_name}-{self.uuid}'
+    def make_referral_link(self):
+        email_initial = str(self.email).split('@')[0]
+        return f'{email_initial}-{self.uuid}'
 
     def __str__(self):
-        return f"({self.user.name},{self.uuid},{self.email})"
+        return f"({self.email},{self.uuid})"
 
 
 class Applicant(models.Model):
@@ -39,8 +38,8 @@ class Applicant(models.Model):
         return f'{self.user}'
 
 
-def post_save_userGroup(sender, instance, *args, **kwargs):
-    if not instance.groups.exists():
+def post_save_userGroup(sender, instance, created, *args, **kwargs):
+    if not created and not instance.groups.exists():
         if instance.is_superuser:
             group_name = 'HR'
         else:
@@ -49,4 +48,10 @@ def post_save_userGroup(sender, instance, *args, **kwargs):
         instance.groups.add(group)
 
 
+def pre_save_employee_trial(sender, instance, *args, **kwargs):
+    if not instance.referral_link:
+        instance.referral_link = instance.make_referral_link()
+
+
 post_save.connect(post_save_userGroup, sender=User)
+pre_save.connect(pre_save_employee_trial, sender=Employee)
