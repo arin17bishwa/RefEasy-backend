d545fb346e629df096cb1f361c4def80423296b1
bishwajit <arin17bishwa@gmail.com>
2022-05-20 16:40:49 +0530

    modified admin panel

--- RefEasyBackend/Users/admin.py
+++ RefEasyBackend/Users/admin.py
@@ -3,18 +3,31 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import *
 
+
 # Register your models here.
 
 
-# class EmployeeAdmin(UserAdmin):
-#     list_display = ('name', 'email', 'uuid', 'role')  # What to display as columns
-#     search_fields = ('email', 'name', '')  # what to search by
-#     readonly_fields = ('last_login', 'date_joined')  # Non-editable fields
-#     ordering = ['name']
-#
-#     filter_horizontal = ()
-#     list_filter = ('role',)
-#     fieldsets = ()
+class EmployeeAdmin(UserAdmin):
+    list_display = ('email', 'uuid', 'referral_link', 'role')  # What to display as columns
+    search_fields = ('email',)  # what to search by
+    readonly_fields = ('last_login', 'date_joined')  # Non-editable fields
+    ordering = ['date_joined']
+
+    filter_horizontal = ()
+    list_filter = ('role',)
+    fieldsets = ()
 
 
-admin.site.register((Employee, Applicant))
+class ApplicantAdmin(UserAdmin):
+    list_display = ('email', 'role')  # What to display as columns
+    search_fields = ('email',)  # what to search by
+    readonly_fields = ('last_login', 'date_joined')  # Non-editable fields
+    ordering = ['date_joined']
+
+    filter_horizontal = ()
+    list_filter = ('role',)
+    fieldsets = ()
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Applicant, ApplicantAdmin)
