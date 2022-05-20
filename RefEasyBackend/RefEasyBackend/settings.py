a63a6a6edaac62333ec9c24f972492ebe3a928b8
bishwajit <arin17bishwa@gmail.com>
2022-05-21 02:14:12 +0530

    increased default page size

--- RefEasyBackend/RefEasyBackend/settings.py
+++ RefEasyBackend/RefEasyBackend/settings.py
@@ -89,7 +89,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3,
+    'PAGE_SIZE': 8,
 
     'DEFAULT_THROTTLE_RATES': {
         'anon': '4/day',
