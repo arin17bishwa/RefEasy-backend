64b5a241d80cd8a5369547614ca54505786ea31f
bishwajit <arin17bishwa@gmail.com>
2022-05-21 19:36:53 +0530

    allowed hosts and cors

--- RefEasyBackend/RefEasyBackend/settings.py
+++ RefEasyBackend/RefEasyBackend/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,8 +25,8 @@ SECRET_KEY = 'django-insecure-b9%49m+%^_+zq7_-%z@urp_bby#$^nygwb*$_(_ubx&2i(ru()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
