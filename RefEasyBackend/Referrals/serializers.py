029417eb5b9be4b4979359689045d65830ad4cca
bishwajit <arin17bishwa@gmail.com>
2022-05-21 22:11:41 +0530

    modified referral serializer

--- RefEasyBackend/Referrals/serializers.py
+++ RefEasyBackend/Referrals/serializers.py
@@ -3,10 +3,16 @@ from pyexpat import model
 from rest_framework import serializers
 
 from .models import Referral
+from Jobs.serializers import JobSerializer
+from Users.serializers import ApplicantSerializer, EmployeeSerializer
 
 
 class ReferralSerializer(serializers.ModelSerializer):
+    job = JobSerializer()
+    ref_emp = EmployeeSerializer()
+    applicant = ApplicantSerializer()
+
     class Meta:
         model = Referral
         fields = '__all__'
-        read_only_fields = ('created_at', 'updated_at', 'slug')
+        read_only_fields = ('created_at', 'updated_at', 'slug', 'job', 'ref_emp', 'applicant')
