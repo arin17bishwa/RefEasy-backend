01438aa177769419a5e37a4a579974bd0320bed2
bishwajit <arin17bishwa@gmail.com>
2022-05-21 21:36:20 +0530

    reformat

--- RefEasyBackend/Referrals/views.py
+++ RefEasyBackend/Referrals/views.py
@@ -15,6 +15,8 @@ from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
 from django.core.mail import send_mail
+
+
 # Create your views here.
 
 
@@ -26,35 +28,39 @@ class ListAllReferrals(generics.ListCreateAPIView):
     def get_queryset(self):
         return Referral.objects.all()
 
+
 class ReferralsCreateView(APIView):
     methods = ['POST']
     permission_classes = (IsAuthenticated,)
+
     def post(self, request, jobid):
         user = self.request.user
         if user.groups.first().name != 'APP':
             return Response({'error': 'Employee cannot apply. Applicants have to apply'},
                             status=status.HTTP_401_UNAUTHORIZED)
         # body_unicode = request.body.decode('utf-8')
-        body = json.loads(request.body)  
+        body = json.loads(request.body)
         ref_link = body['referral_link']
         print(ref_link)
-        job = Job.objects.get(id = jobid)
-        applicant = Applicant.objects.get(user = user)
+        job = Job.objects.get(id=jobid)
+        applicant = Applicant.objects.get(user=user)
         ref_emp = Employee.objects.get(referral_link=ref_link)
         referral = Referral(job=job, ref_emp=ref_emp, applicant=applicant, status="L01")
         referral.save()
         return Response(status=status.HTTP_200_OK)
 
+
 class GetReferralLink(APIView):
     methods = ['GET']
     permission_classes = (IsAuthenticated,)
+
     def get(self, request):
         user = self.request.user
         if user.groups.first().name == 'APP':
-            return Response({'error':'Applicants cannot refer'}, status=status.HTTP_401_UNAUTHORIZED)   
-        emp = Employee.objects.get(user=user)       
+            return Response({'error': 'Applicants cannot refer'}, status=status.HTTP_401_UNAUTHORIZED)
+        emp = Employee.objects.get(user=user)
         return Response({'referral_link': emp.referral_link},
-                            status=status.HTTP_200_OK)
+                        status=status.HTTP_200_OK)
 
 
 class TrackMyReferral(APIView):
@@ -78,26 +84,27 @@ class ReferralsUpdateView(APIView):
     methods = ['POST']
     permission_classes = (IsAuthenticated,)
     serializer_class = ReferralSerializer
+
     def post(self, request):
         user = self.request.user
         if user.groups.first().name != 'HR':
-            return Response({"error":"Since not a HR, cannot update status" })
-        body = json.loads(request.body)  
+            return Response({"error": "Since not a HR, cannot update status"})
+        body = json.loads(request.body)
         job_slug = body['job_slug']
         app_email = body['app_email']
         sts = body['status']
         print(app_email)
 
-        job = Job.objects.get(slug = job_slug)
-        app = Applicant.objects.get(email = app_email)
-        referral = Referral.objects.get(job = job, applicant = app)
+        job = Job.objects.get(slug=job_slug)
+        app = Applicant.objects.get(email=app_email)
+        referral = Referral.objects.get(job=job, applicant=app)
         print(referral.status)
         print(sts)
-        
+
         # if referral.status == sts:
         #     return Response(status=status.HTTP_200_OK)
-        
-        #TODO: Send email to ref_emp and applicant that their status is updated
+
+        # TODO: Send email to ref_emp and applicant that their status is updated
         referral.status = sts
         referral.save()
 
@@ -119,8 +126,3 @@ class ReferralsUpdateView(APIView):
         print(referral.ref_emp.email)
         print(app.email)
         return Response({"msg": "updated"}, status=status.HTTP_200_OK)
-
-
-
-
-    
