8d16a82dcf13658ea5d9c51a900e3fbfee168015
bishwajit <arin17bishwa@gmail.com>
2022-05-22 23:44:59 +0530

    created separate serializer for listing and creation

--- RefEasyBackend/Jobs/serializers.py
+++ RefEasyBackend/Jobs/serializers.py
@@ -10,21 +10,28 @@ POS_MAPPING = {i: j for i, j in Job.POS_CHOICES}
 
 
 class JobSerializer(serializers.ModelSerializer):
-    # department = serializers.SerializerMethodField()
-    # location = serializers.SerializerMethodField()
-    # position_type = serializers.SerializerMethodField()
+    class Meta:
+        model = Job
+        fields = '__all__'
+        read_only_fields = ('created_at', 'last_edit', 'slug')
+
+
+class JobListSerializer(serializers.ModelSerializer):
+    department = serializers.SerializerMethodField()
+    location = serializers.SerializerMethodField()
+    position_type = serializers.SerializerMethodField()
 
     class Meta:
         model = Job
         fields = '__all__'
         read_only_fields = ('created_at', 'last_edit', 'slug')
 
-    # def get_department(self, obj):
-    #     print(obj)
-    #     return DEPT_MAPPING[obj.department]
-    #
-    # def get_location(self, obj):
-    #     return LOC_MAPPING[obj.location]
-    #
-    # def get_position_type(self, obj):
-    #     return POS_MAPPING[obj.position_type]
+    def get_department(self, obj):
+        print(obj)
+        return DEPT_MAPPING[obj.department]
+
+    def get_location(self, obj):
+        return LOC_MAPPING[obj.location]
+
+    def get_position_type(self, obj):
+        return POS_MAPPING[obj.position_type]
